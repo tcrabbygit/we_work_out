3c9fac66890114dbaef752130f854f92ddbc3b73
tcrabtree-fsly <tcrabtree@fastly.com>
2023-07-13 15:24:02 -0400

    Update prep_by_name
    do dtype change at end of function

--- tracker_app.py
+++ tracker_app.py
@@ -58,13 +58,15 @@ if len(row_updates) > 0:
     df = df.drop_duplicates().sort_values(by="Day").reset_index(drop=True)
 
 df["Day"] = pd.to_datetime(df["Day"])
-df["Week Date"] = pd.to_datetime(df["Week Date"])
+# df["Week Date"] = pd.to_datetime(df["Week Date"])
 df["Minutes"] = df["Minutes"].astype(int)
 df["Distance"] = df["Distance"].astype(float)
 df["Week"] = df["Week"].astype(int)
 
 lauren = prep_by_name(df, "Lauren")
 tara = prep_by_name(df, "Tara")
+
+df["Week Date"] = pd.to_datetime(df["Week Date"])
 
 combined = lauren.merge(tara, on=["Week", "Week Date"], suffixes=["_l", "_t"]).rename(columns={"Minutes_l": "Minutes (Lauren)", "Minutes_t": "Minutes (Tara)", "Workouts_l": "Workouts (Lauren)", "Workouts_t": "Workouts (Tara)", "Distance_l": "Distance (Lauren)", "Distance_t": "Distance (Tara)", "Points_l": "Points (Lauren)", "Points_t": "Points (Tara)"})
 combined["Winner"] = combined.apply(lambda row: "None :(" if (row["Minutes (Lauren)"] < 90 and row["Minutes (Tara)"] < 90) or (row["Workouts (Lauren)"] < 3 and row["Workouts (Tara)"] < 3) else ("Lauren" if (row["Minutes (Lauren)"] * row["Workouts (Lauren)"]) > (row["Minutes (Tara)"] * row["Workouts (Tara)"]) else ("Tara" if (row["Minutes (Lauren)"] * row["Workouts (Lauren)"]) < (row["Minutes (Tara)"] * row["Workouts (Tara)"]) else "Tie")), axis=1)
